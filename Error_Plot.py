b7138a86bdde357dd3bcbffc9505c8dc7f6de515
eqnmapper <83826245+eqnmapper@users.noreply.github.com>
2023-04-08 19:04:50 +0200

    Update and rename Error_Plot_IID.py to Error_Plot.py

--- Error_Plot.py
+++ Error_Plot.py
@@ -17,7 +17,7 @@ used_algorithms = ['Unconstrained_Wiring','HWES_set_integer_exponents',
                    'SBF_set_integer_exponents','SBF_set_integer_exponents_not_codebook_weighted',
                    'Greedy_set_integer_exponents','Greedy_set_integer_exponents_not_codebook_weighted']
 
-#determine the file name of each algorithm
+#determine the file name of each algorithm for iid case
 
 data_paths = []
 for alg in used_algorithms:
@@ -79,5 +79,68 @@ plt.xlabel('Wiring Layer')
 plt.xticks(np.arange(0,11,1),np.arange(0,11,1))
 plt.legend()
 plt.savefig('MSE_IID_sparsity_3', dpi=200)
+
+#determine the file name of each algorithm for non-iid case
+
+data_paths = []
+for alg in used_algorithms:
+    data_paths.append(f'Target_Wiring_UNN_Constr_Quant_C_{alg}_sparsity_3_non_IID.npz')
+
+
+wiring_depth = 10
+number_datasets = len(data_paths)
+number_simulations = 1000
+
+wiring_exponent_initial_codebook = np.arange(-6,3,dtype=float)
+
+MSE = np.zeros((number_datasets,wiring_depth+1))
+
+
+
+for idx, path in enumerate(data_paths):
+
+    simulation_data = np.load(path)
+
+    wiring_mats = simulation_data.get('Wiring_Mats')
+
+    target_mats = simulation_data.get('Target_Mats')
+
+    #calculate norm of error between columns of target matrix and approximated target matrix
+    N = target_mats.shape[1]
+    K = target_mats.shape[2]
+
+    Squared_Error = np.zeros((number_simulations,wiring_depth+1,N,K))
+
+    for i in range(number_simulations):
+        B = quantize_nearest_power_2_finite_set(target_mats[i],wiring_exponent_initial_codebook)
+            
+        T_approx = B
+        Squared_Error[i,0] = (target_mats[i]-T_approx)**2
+        for j in range(wiring_depth):
+            T_approx = T_approx@wiring_mats[i,j]
+            #norm_column_error[i,j+1] = np.linalg.norm((target_mats[i]-T_approx),axis = 0)
+            Squared_Error[i,j+1] = (target_mats[i]-T_approx)**2
+
+    MSE[idx,:] = np.mean(Squared_Error,axis=(0,2,3))
+
+    
+    del target_mats
+
+    del wiring_mats
+
+
+
+
+plt.figure(figsize=(16,9))
+layers = np.arange(0,wiring_depth+1)
+
+for idx, alg in enumerate(used_algorithms):
+    plt.plot(layers,np.log2(MSE[idx]),label=alg)
+
+plt.ylabel('$\mathrm{log_2}(\mathrm{MSE})$')
+plt.xlabel('Wiring Layer')
+plt.xticks(np.arange(0,11,1),np.arange(0,11,1))
+plt.legend()
+plt.savefig('MSE_non_IID_sparsity_3', dpi=200)
 
 
